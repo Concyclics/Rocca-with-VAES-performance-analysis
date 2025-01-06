6c5b98cf2c02376090a71ef3a9be451fbeed641f
CHEN Han <33325023+Concyclics@users.noreply.github.com>
2025-01-06 11:28:08 +0800

    Update compile_x86.sh

--- test/compile_x86.sh
+++ test/compile_x86.sh
@@ -1,2 +1,5 @@
 gcc -O3 -march=native HiAE/*.c -o HiAE -w
 gcc -O3 -march=native aegis/*.c -o AEGIS -w
+gcc -O3 -march=native snow-v/*.c -o SNOW-V -w
+gcc -O3 -march=native rocca/rocca.c -o ROCCA -w
+gcc -O3 -march=native rocca/rocca_s.c -o ROCCA-S -w
