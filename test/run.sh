936b7e4194b16f2c6e553541be7279c6df459b96
CHEN Han <33325023+Concyclics@users.noreply.github.com>
2025-01-06 11:33:52 +0800

    Create run.sh

---
+++ test/run.sh
@@ -0,0 +1,21 @@
+./HiAE > HiAE_log_1
+./HiAE > HiAE_log_2
+./HiAE > HiAE_log_3
+./AEGIS > AEGIS_log_1
+./AEGIS > AEGIS_log_2
+./AEGIS > AEGIS_log_3
+./SNOW-V > SNOW-V_log_1
+./SNOW-V > SNOW-V_log_2
+./SNOW-V > SNOW-V_log_3
+./ROCCA > ROCCA_log_1
+./ROCCA > ROCCA_log_2
+./ROCCA > ROCCA_log_3
+./ROCCA-S > ROCCA-S_log_1
+./ROCCA-S > ROCCA-S_log_2
+./ROCCA-S > ROCCA-S_log_3
+openssl speed -evp AES-256-CTR > AES-256-CTR_log_1
+openssl speed -evp AES-256-CTR > AES-256-CTR_log_2
+openssl speed -evp AES-256-CTR > AES-256-CTR_log_3
+openssl speed -aead -evp AES-256-GCM > AES-256-GCM_log_1
+openssl speed -aead -evp AES-256-GCM > AES-256-GCM_log_2
+openssl speed -aead -evp AES-256-GCM > AES-256-GCM_log_3
