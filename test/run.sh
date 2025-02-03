b77c4d8d69ce33d380f55a817c14a51bbb4e4938
CHEN Han <33325023+Concyclics@users.noreply.github.com>
2025-02-03 17:24:22 +0800

    Update run.sh

--- test/run.sh
+++ test/run.sh
@@ -1,21 +1,30 @@
-for i in {1..10} 
+echo "Cipher Benchmark" > bench_log
+for i in {1..50} 
 do
-    ./HIAE_run > HiAE_log_$i
-    ./AEGIS_run > AEGIS_log_$i
-    ./SNOW-V_run > SNOW-V_log_$i
-    ./ROCCA_run > ROCCA_log_$i
-    ./ROCCA-S_run > ROCCA-S_log_$i
+    echo "Round" $i >> bench_log
+    echo "bench HiAE" >> bench_log
+    ./HIAE_run >> bench_log
+    echo "bench AEGIS" >> bench_log
+    ./AEGIS_run >> bench_log
+    echo "bench SNOW-V" >> bench_log
+    ./SNOW-V_run >> bench_log
+    echo "bench ROCCA" >> bench_log
+    ./ROCCA_run >> bench_log
+    echo "bench ROCCA-S" >> bench_log
+    ./ROCCA-S_run >> bench_log
     echo "Finished loop $i"
 done
 
-for i in {1..3}
+echo "Openssl AES" > AES_log
+for i in {1..10}
 do
-    openssl speed -bytes 16384 -evp aes-256-ctr > AES-256-CTR_log_$i
-    openssl speed -bytes 16384 -evp aes-256-gcm > AES-256-GCM_log_$i
+    echo "Round" $i >> AES_log
+    openssl speed -bytes 16384 -evp aes-256-ctr >> AES_log
+    openssl speed -bytes 16384 -evp aes-256-gcm >> AES_log
     for len in 8192 4096 2048 1024 256 64
     do
-        openssl speed -bytes $len -evp aes-256-ctr >> AES-256-CTR_log_$i
-        openssl speed -bytes $len -evp aes-256-gcm >> AES-256-GCM_log_$i
+        openssl speed -bytes $len -evp aes-256-ctr >> AES_log
+        openssl speed -bytes $len -evp aes-256-gcm >> AES_log
     done
 done
 echo "Finished"
