a64674667c880c0c811531571b037e33983ca277
Concyclics <1224763438@qq.com>
2025-02-10 00:49:54 +0800

    update

--- test/compile_apple.sh
+++ test/compile_apple.sh
@@ -1,6 +1,6 @@
-gcc-14 -O3 -march=armv8.6-a+crypto HiAE/*.c -o HIAE_run -w
-gcc-14 -O3 -march=armv8.6-a+crypto aegis/*.c -o AEGIS_run -w
-gcc-14 -O3 -march=armv8.6-a+crypto snow-v/*.c -o SNOW-V_run -w
-gcc-14 -O3 -march=armv8.6-a+crypto rocca/rocca.c -o ROCCA_run -w
-gcc-14 -O3 -march=armv8.6-a+crypto rocca/rocca_s.c -o ROCCA-S_run -w
+gcc-14 -O3 -march=armv8-a+crypto HiAE/*.c -o HIAE_run -w
+gcc-14 -O3 -march=armv8-a+crypto aegis/*.c -o AEGIS_run -w
+gcc-14 -O3 -march=armv8-a+crypto snow-v/*.c -o SNOW-V_run -w
+gcc-14 -O3 -march=armv8-a+crypto rocca/rocca.c -o ROCCA_run -w
+gcc-14 -O3 -march=armv8-a+crypto rocca/rocca_s.c -o ROCCA-S_run -w
 echo "Compilation done"
